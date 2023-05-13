30e19cb3ee989785ad8d67d1af3a4abae6a0e931
AmreetKumarkhuntia <amreetleo123@gmail.com>
2023-05-14 00:20:32 +0530

    updated models

--- server.js
+++ server.js
@@ -1,14 +1,54 @@
 const express = require('express');
 const app = express();
 const cors=require('cors');
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node');
 
 const port=2000;
 
+app.use(cors());
+
+
+//MODEL LOADING USING TENSORFLOW
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//EXAMPLE PREDICTIONS
+
+async function loadModel() {
+    const model = await tf.loadLayersModel('file://models/model.json');
+    model.summary();
+
+    // Create an input tensor
+    const input = tf.tensor2d(["i love you"], [1, 1]);
+
+    // Make a prediction
+    const prediction = model.predict(input);
+
+    // Log the prediction
+    prediction.print();
+
+  }
+  
+loadModel();
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+app.get('/', function(req, res) {
+    res.send("hello world!");
 
 
 
+});
+
+app.get('/sentiment', (req,res)=>{
+
+    var sentiment;
+
+    req.query.sentence?sentiment = req.query.sentence:"";
 
 
+
+    res.send(sentiment);
+});
 
 
 app.listen(port,()=>{
